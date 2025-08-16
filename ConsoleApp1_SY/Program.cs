6a60e3683fc21043c2493eb0c48a3e060ad44090
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-08-16 14:28:16 -0500

    simplified code with restructuring. need improvements yet of actual calculations and additional "added difficulty" items yet.

--- ConsoleApp1_SY/Program.cs
+++ ConsoleApp1_SY/Program.cs
@@ -11,12 +11,11 @@ class Program
         Console.Write("Enter the cost of one unit of flooring: $");
         decimal cost = decimal.Parse(Console.ReadLine());
         
-        //Verifying if user as an irregular flooring plan to purchase flooring for.
-        Console.WriteLine("Do you have a irregular floor? Y/N");
-        bool isIrregular = Console.ReadLine().ToLower() == "y";
-        
-        if (isIrregular == false)
-        {   
+        // Asking user on floor plan
+        Console.Write("What is the shape of your room?");
+        string roomShape = Console.ReadLine();
+        if (roomShape == "square")
+        {
             // User inputs width
             Console.Write("Enter the width needed: ");
             decimal unitWidth = decimal.Parse(Console.ReadLine());
@@ -28,34 +27,31 @@ class Program
             // Outputs the calculation of cost needed to purchase enough flooring for area
             decimal calculationOfArea = unitWidth * unitLength * cost;
             Console.WriteLine($"The cost of your flooring would be: ${calculationOfArea}");
-        }       
-        else {
-        // Verifying if user has triangular floor or a circular floor and then doing calculations for each.
-        Console.WriteLine("Do you have a triangle floor? Y/N");
-        bool isTriangle = Console.ReadLine().ToLower() == "y";
-        
-        if (isTriangle == false)
+        }
+        if (roomShape == "triangular")
         {
+            Console.Write("Input base of triangular flooring: ");
+            double triangularBase = double.Parse(Console.ReadLine());
+        
+            Console.Write("Input height of triangular flooring: ");
+            double triangularHeight = double.Parse(Console.ReadLine());
+            double formulaAreaOfTriangle = .5 * (triangularBase * triangularHeight);
+        
+            Console.WriteLine($"The cost of your flooring would be: ${formulaAreaOfTriangle}");
+        }
+        
+        if (roomShape == "circular"){
             Console.Write("Input radius of circular floor: ");
             decimal radius = decimal.Parse(Console.ReadLine());
             decimal pi = 3.14159265358979323846m;
             decimal formulaAreaOfCircle = pi * (radius * radius);
             decimal calculatedCostOfFlooring = formulaAreaOfCircle * cost;
             Console.WriteLine($"The cost of your flooring would be: ${calculatedCostOfFlooring}");
-        }
-        else {
-        Console.Write("Input base of triangular flooring: ");
-        double triangularBase = double.Parse(Console.ReadLine());
-        
-        Console.Write("Input height of triangular flooring: ");
-        double triangularHeight = double.Parse(Console.ReadLine());
-        double formulaAreaOfTriangle = .5 * (triangularBase * triangularHeight);
-        
-        Console.WriteLine($"The cost of your flooring would be: ${formulaAreaOfTriangle}");
-        }
-        /* So I have tried decimal to get more of a precise calculation.
-        I believe that would be the correct route to go since we are working with cost.
-        What could I add to make the finished calculation more in a standard currency amount with only 2 placements after the decimal? */   
-    }
+            
+            /* So I have tried decimal to get more of a precise calculation.
+       I believe that would be the correct route to go since we are working with cost.
+       What could I add to make the finished calculation more in a standard currency amount with only 2 placements after the decimal? */   
+
+       }
     }
 }
