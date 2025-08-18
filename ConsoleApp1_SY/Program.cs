c93c32bac4598972e37393708d9b2af3d10c82c8
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-08-18 15:10:08 -0500

    updated program.cs file
    somehow my code got reverted back to much earlier and without new implementations of code. Saving and updating to not lose it again.

--- ConsoleApp1_SY/Program.cs
+++ ConsoleApp1_SY/Program.cs
@@ -11,10 +11,24 @@ class Program
         Console.Write("Enter the cost of one unit of flooring: $");
         decimal cost = decimal.Parse(Console.ReadLine());
         
+        const decimal YARDS_PER_HOUR = 20;
+        const decimal HOURLY_RATE = 86.00M;
+        const decimal TOTAL_HOURS = squareFootage/YARDS_PER_HOUR;
+        decimal totalLaborCost = TOTAL_HOURS * HOURLY_RATE;
+        
         // Asking user on floor plan
-        Console.Write("What is the shape of your room?");
+        Console.Write("What is the shape of your room? square | triangular | circular\n");
         string roomShape = Console.ReadLine();
-        if (roomShape == "square")
+        const string TRIANGULAR_SHAPE_TYPE = "triangular";
+        const string SQUARE_SHAPE_TYPE = "square";
+        const string CIRCULAR_SHAPE_TYPE = "circular";
+        
+        if (roomShape == null)
+        {
+            Console.WriteLine("You must enter a valid room shape.");
+        }
+        
+        if (roomShape == SQUARE_SHAPE_TYPE)
         {
             // User inputs width
             Console.Write("Enter the width needed: ");
@@ -23,35 +37,40 @@ class Program
             // User inputs length
             Console.Write("Enter the length needed: ");
             decimal unitLength = decimal.Parse(Console.ReadLine());
+            decimal squareFootage = unitWidth * unitLength;
+            decimal squareFlooringCost = squareFootage * cost;
+            decimal totalHours = squareFlooringCost * YARDS_PER_HOUR;
+            Console.WriteLine($"The cost of your flooring would be: ${squareFlooringCost}");
+            
+            Console.WriteLine($"The estimated cost of labor for {squareFootage} units of sq. ft. is ${totalLaborCost}");
             
             // Outputs the calculation of cost needed to purchase enough flooring for area
             decimal calculationOfArea = unitWidth * unitLength * cost;
             Console.WriteLine($"The cost of your flooring would be: ${calculationOfArea}");
         }
-        if (roomShape == "triangular")
+        if (roomShape == TRIANGULAR_SHAPE_TYPE)
         {
             Console.Write("Input base of triangular flooring: ");
             double triangularBase = double.Parse(Console.ReadLine());
         
             Console.Write("Input height of triangular flooring: ");
             double triangularHeight = double.Parse(Console.ReadLine());
-            double formulaAreaOfTriangle = .5 * (triangularBase * triangularHeight);
-        
-            Console.WriteLine($"The cost of your flooring would be: ${formulaAreaOfTriangle}");
+            double triangleFootage = .5 * (triangularBase * triangularHeight);
+            double triangleFlooringCost = triangleFootage * (double)cost;
+            
+            Console.WriteLine($"The cost of your flooring would be: ${triangleFlooringCost}");
+            double totalHours = triangleFootage / (double)YARDS_PER_HOUR;
+            Console.WriteLine($"The estimated cost of labor for {triangleFootage} units of sq. ft. is ${totalLaborCost}");
         }
         
-        if (roomShape == "circular"){
+        if (roomShape == CIRCULAR_SHAPE_TYPE){
             Console.Write("Input radius of circular floor: ");
             decimal radius = decimal.Parse(Console.ReadLine());
             decimal pi = 3.14159265358979323846m;
-            decimal formulaAreaOfCircle = pi * (radius * radius);
-            decimal calculatedCostOfFlooring = formulaAreaOfCircle * cost;
-            Console.WriteLine($"The cost of your flooring would be: ${calculatedCostOfFlooring}");
-            
-            /* So I have tried decimal to get more of a precise calculation.
-       I believe that would be the correct route to go since we are working with cost.
-       What could I add to make the finished calculation more in a standard currency amount with only 2 placements after the decimal? */   
-
+            decimal circularFootage = pi * (radius * radius);
+            decimal circularFlooringCost = circularFootage * cost;
+            Console.WriteLine($"The cost of your flooring would be: ${circularFlooringCost}");
+            Console.WriteLine($"The estimated cost of labor for {circularFootage} units of sq. ft. is ${totalLaborCost}");
        }
     }
 }
